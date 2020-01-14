99262d46090311bbf07fc7bf5f37bddb80bbe7fd
Zane Suhadolnik <zanesuhi@gmail.com>
2020-01-14 06:26:14 +0100

    [NC] Further dropdown styling and positioning

--- src/components/navbar-dropdown.tsx
+++ src/components/navbar-dropdown.tsx
@@ -1,10 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
+const Dropdown = styled.div`
+  display: inline-block;
+`
+
+const DropdownSelect = styled.select`
+  background-color: black;
+  color: white;
+  border: solid black 1px;
+`
+
 const NavbarDropdown: React.FC = () => {
-  const [state, setState] = useState();
   return (
-    <div></div>
+    <Dropdown>
+      <DropdownSelect name="pets">
+        <option value=""></option>
+        <option value="dog">Dog</option>
+        <option value="cat">Cat</option>
+        <option value="hamster">Hamster</option>
+        <option value="parrot">Parrot</option>
+        <option value="spider">Spider</option>
+        <option value="goldfish">Goldfish</option>
+      </DropdownSelect>
+    </Dropdown>
   )
 }
 
