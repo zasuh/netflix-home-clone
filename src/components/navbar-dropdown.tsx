6fea67e3bff0c05536fcb6f06f0ffe2179f9fb78
Zane Suhadolnik <zanesuhi@gmail.com>
2020-01-15 06:33:15 +0100

    [NC] Added: dropdown is now a component

--- src/components/navbar-dropdown.tsx
+++ src/components/navbar-dropdown.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Dropdown = styled.div`
   display: inline-block;
 `
 
-const DropdownSelect = styled.select`
+const DropdownSelect = styled.div`
   background-color: black;
   color: white;
   border: solid black 1px;
 `
 
 const NavbarDropdown: React.FC = () => {
+  const [menu, setMenu] = useState(false);
   return (
     <Dropdown>
-      <DropdownSelect name="pets">
-        <option value=""></option>
-        <option value="dog">Dog</option>
-        <option value="cat">Cat</option>
-        <option value="hamster">Hamster</option>
-        <option value="parrot">Parrot</option>
-        <option value="spider">Spider</option>
-        <option value="goldfish">Goldfish</option>
-      </DropdownSelect>
+      <div onClick={() => setMenu(!menu)}>Show menu</div>
+      {menu ? (
+        <DropdownSelect>
+          <button>Item1</button>
+          <button>Item2</button>
+          <button>Item3</button>
+        </DropdownSelect>
+      ) : null }
     </Dropdown>
   )
 }
