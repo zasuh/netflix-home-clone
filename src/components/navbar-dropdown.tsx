1634aa4de89930626287f58874adf25c8d658c80
Zane Suhadolnik <zanesuhi@gmail.com>
2020-01-16 06:46:22 +0100

    [NC] Added: dropdown styling and positioning

--- src/components/navbar-dropdown.tsx
+++ src/components/navbar-dropdown.tsx
@@ -6,16 +6,35 @@ const Dropdown = styled.div`
 `
 
 const DropdownSelect = styled.div`
+  position: absolute;
+  right: 10px;
+  top: 60px;
   background-color: black;
   color: white;
   border: solid black 1px;
+  button {
+    display: block;
+    margin: 10px 0 10px 0;
+    background-color: black;
+    color: white;
+    border: solid black 1px;
+    width: 90px;
+  }
+`
+
+const DropdownTriangle = styled.div`
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 10px 7.5px 0 7.5px;
+  border-color: #ffffff transparent transparent transparent;
 `
 
 const NavbarDropdown: React.FC = () => {
   const [menu, setMenu] = useState(false);
   return (
     <Dropdown>
-      <div onClick={() => setMenu(!menu)}>Show menu</div>
+      <DropdownTriangle onClick={() => setMenu(!menu)}></DropdownTriangle>
       {menu ? (
         <DropdownSelect>
           <button>Item1</button>
