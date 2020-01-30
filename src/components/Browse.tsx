cc81652db952506b371d03114617de271dfbc8fc
Zane Suhadolnik <zanesuhi@gmail.com>
2020-01-30 06:54:07 +0100

    Updated

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,11 +1,37 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const BrowseWrapper = styled.div`
+  display: inline-block;
+`
+
+const BrowseTitle = styled.h3`
+  display: inline-block;
+  color: white;
+  padding: 0 0 0 20px;
+`
+
+const BrowseWindow = styled.div`
+  position: absolute;
+  left: 230px;
+  width: 500px;
+  height: 300px;
+  background-color: black;
+  color: white;
+  border: solid white 1px;
+`
 
 const Browse: React.FC = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div onClick={() => {setOpen(true)}}>
-      {open ? <h4>Hello</h4> : null}
-    </div>
+    <BrowseWrapper onClick={() => {setOpen(!open)}}>
+      <BrowseTitle>Browse</BrowseTitle>
+      {open ? (
+        <BrowseWindow>
+          Hello
+        </BrowseWindow>
+      ) : null}
+    </BrowseWrapper>
   )
 }
 
