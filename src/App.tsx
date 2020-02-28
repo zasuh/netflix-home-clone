bba101ca775357166d98ec7752c412524b8c5dbc
Zane Suhadolnik <zanesuhi@gmail.com>
2020-02-28 06:42:02 +0100

    [NC] Added: Cover image and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 import Navbar from './components/Navbar';
 import './App.css';
+import BOJ from './assets/bojack3.jpg';
+import LOGO from './assets/bojlogo.png';
+
+const ImageWrapper = styled.div`
+  position: relative;
+  top: 0;
+  left: 0;
+`
+
+const ImageHeader = styled.img`
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 1905px;
+  height: 1000px;
+`
+
+const ImageLogo = styled.img`
+  position: absolute;
+  top: 30px;
+  left: 30px;
+`
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Navbar />
+      <ImageWrapper>
+        <ImageHeader src={BOJ} alt="Bojack Horseman on balcony"/>
+        <ImageLogo src={LOGO} alt="Bojack Horseman logo"/>
+      </ImageWrapper>
     </div>
   );
 }
