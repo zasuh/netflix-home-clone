2ec7791dbd81fdfff317879b3d8696280c7104ed
Zane Suhadolnik <zanesuhi@gmail.com>
2020-03-01 06:14:15 +0100

    [NC] Added: Show description

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Navbar from './components/Navbar';
 import './App.css';
+
 import BOJ from './assets/bojack3.jpg';
 import LOGO from './assets/bojlogo.png';
 
@@ -9,6 +10,14 @@ const ImageWrapper = styled.div`
   position: relative;
   top: 0;
   left: 0;
+
+  h4 {
+    position: absolute;
+    top: 270px;
+    left: 50px;
+    max-width: 600px;
+    color: white;
+  }
 `
 
 const ImageHeader = styled.img`
@@ -25,6 +34,14 @@ const ImageLogo = styled.img`
   left: 30px;
 `
 
+const ShowDesc = styled.p`
+  position: absolute;
+  top: 300px;
+  left: 50px;
+  max-width: 600px;
+  color: white;
+`
+
 const App: React.FC = () => {
   return (
     <div className="App">
@@ -32,6 +49,8 @@ const App: React.FC = () => {
       <ImageWrapper>
         <ImageHeader src={BOJ} alt="Bojack Horseman on balcony"/>
         <ImageLogo src={LOGO} alt="Bojack Horseman logo"/>
+        <h4>Watch the final episodes now</h4>
+        <ShowDesc>He's a half-horse, half-man, has-been TV star who drinks a bit too much. He'r really got a lot going right now.</ShowDesc>
       </ImageWrapper>
     </div>
   );
