e9544a073beaf763293d94aa73fa0d6a7b19eaa8
Zane Suhadolnik <zanesuhi@gmail.com>
2020-03-02 06:40:21 +0100

    [NC] Added: Header buttons

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const ImageWrapper = styled.div`
   top: 0;
   left: 0;
 
-  h4 {
+  h3 {
     position: absolute;
     top: 270px;
     left: 50px;
@@ -40,6 +40,33 @@ const ShowDesc = styled.p`
   left: 50px;
   max-width: 600px;
   color: white;
+  font-size: 24px;
+`
+
+const HeaderButton = styled.a`
+  display:inline-block;
+  padding:0.35em 1.2em;
+  border:0.1em solid #FFFFFF;
+  margin:0 0.3em 0.3em 0;
+  border-radius:0.12em;
+  box-sizing: border-box;
+  text-decoration:none;
+  font-family:'Roboto',sans-serif;
+  font-weight:300;
+  color:#FFFFFF;
+  text-align:center;
+  transition: all 0.2s;
+
+  &:hover{
+    color: #000;
+    background-color: #FFF;
+  }
+`
+
+const ButtonWrapper = styled.div`
+  position: absolute;
+  top: 400px;
+  left: 50px;
 `
 
 const App: React.FC = () => {
@@ -49,8 +76,12 @@ const App: React.FC = () => {
       <ImageWrapper>
         <ImageHeader src={BOJ} alt="Bojack Horseman on balcony"/>
         <ImageLogo src={LOGO} alt="Bojack Horseman logo"/>
-        <h4>Watch the final episodes now</h4>
+        <h3>Watch the final episodes now</h3>
         <ShowDesc>He's a half-horse, half-man, has-been TV star who drinks a bit too much. He'r really got a lot going right now.</ShowDesc>
+        <ButtonWrapper>
+          <HeaderButton href="something">Watch Now</HeaderButton>
+          <HeaderButton href="something">Add To List</HeaderButton>
+        </ButtonWrapper>
       </ImageWrapper>
     </div>
   );
